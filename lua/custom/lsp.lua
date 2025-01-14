901d7032b6c40426ee426007593edfbf27b7ab40
youngmoon01 <eomyoungmoon01@gmail.com>
2025-01-14 21:29:20 +0900

    feat: add diagnostic shortcut and switch to onedark theme

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -165,7 +165,7 @@ return {
         -- gopls = {},
         pyright = {},
         ruff = {},
-        rust_analyzer = {},
+        rust_analyzer = { diagnostics = { true } },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
