4abf0917aa27b3d7654eda3211862072fa05f912
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-01-14 22:56:59 +0900

    feat: add cursor position memory and adjust window width

--- init.lua
+++ init.lua
@@ -214,6 +214,19 @@ require('lazy').setup({
   },
 })
 
+-- Remember cursor position between sessions
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  pattern = '*',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match '(%S+)%s+%(.-%)'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
