61abfc8b2c5351c15cd9c3cfcf7e94b13cad571a
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-01-17 10:59:43 +0900

    Remove scrolloff option(not my flavor)

--- init.lua
+++ init.lua
@@ -60,9 +60,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
