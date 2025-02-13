a8391abaf17428a3ba431d08deed37f5b54336b0
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-02-13 11:13:02 +0900

    rollback colorscheme to onedark

--- lua/custom/colorscheme.lua
+++ lua/custom/colorscheme.lua
@@ -4,16 +4,17 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'sainnhe/everforest',
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'everforest'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      require('onedark').setup {
+        style = 'cool',
+      }
+      require('onedark').load()
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 }
