7aa554dc05418968993ed9676c280f72e8cf493f
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-02-23 09:25:23 +0900

    mini.statusline(removed git and diff info) customized

--- lua/custom/mini.lua
+++ lua/custom/mini.lua
@@ -20,9 +20,40 @@ return {
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
+      --
+      local active_content = function()
+        local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+        -- local git = MiniStatusline.section_git { trunc_width = 40 }
+        -- local diff = MiniStatusline.section_diff { trunc_width = 75 }
+        local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+        local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+        local filename = MiniStatusline.section_filename { trunc_width = 140 }
+        local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+        local location = MiniStatusline.section_location { trunc_width = 75 }
+        local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+        return MiniStatusline.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          -- { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+          { hl = 'MiniStatuslineDevinfo', strings = { diagnostics, lsp } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl, strings = { search, location } },
+        }
+      end
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          -- Content for active window
+          active = active_content,
+          -- Content for inactive window(s)
+          inactive = nil,
+        },
+      }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
