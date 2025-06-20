7224fd28d4eed8cf459bb75f803348dfc7b64467
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-06-21 00:30:25 +0900

    update claude model to 4

--- lua/custom/avante.lua
+++ lua/custom/avante.lua
@@ -8,7 +8,7 @@ return {
       providers = {
         claude = {
           endpoint = 'https://api.anthropic.com',
-          model = 'claude-3-7-sonnet-20250219',
+          model = 'claude-sonnet-4-20250514',
           extra_request_body = {
             temperature = 0.75,
             max_tokens = 4096,
