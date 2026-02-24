ec2f2f2a810021022b2fba4bcfd349e8191e3301
youngmoon01 <eomyoungmoon01@gmail.com>
2026-02-24 13:24:15 +0900

    update colorscheme to everforest

--- lua/custom/colorscheme.lua
+++ lua/custom/colorscheme.lua
@@ -1,20 +1,15 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'navarasu/onedark.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      require('onedark').setup {
-        style = 'cool',
+  {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').setup {
+        -- Your config here
       }
-      require('onedark').load()
-      vim.cmd.colorscheme 'onedark'
+      require('everforest').load()
     end,
   },
 }
